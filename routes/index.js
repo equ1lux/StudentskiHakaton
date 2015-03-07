bebb946cdc79d6a4241934d86347763d3f58dc9a
dumyan <damjan91@gmail.com>
2015-03-07 17:22:45 +0100

    added index for /

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index2', {
+        title: 'Homepage'
+    });
+});
+
+/* GET test page. */
 router.get('/test', function(req, res, next) {
 	res.render('index0', {
 		title: 'Express'
