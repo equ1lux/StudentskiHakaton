f9201c2cc4dd08513990420364da7196b5e252e6
dumyan <damjan91@gmail.com>
2015-03-08 10:19:21 +0100

    morning bugfixes

--- app.js
+++ app.js
@@ -5,49 +5,66 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 //*MongoDB
 var mongo = require('mongoskin');
-var db = mongo.db("mongodb://dumyan:testestes123@ds053251.mongolab.com:53251/hakmof", 
-	["scraped_data", "trends_data"]);
+var db = mongo.db("mongodb://mirza:0123456789@ds053251.mongolab.com:53251/hakmof", ["scraped_data", "trends_data"]);
+
+
+// Facebook
+var graph = require('fbgraph');
+var token = require('./tokens');
+graph.setAccessToken(token.fb.token);
 
 
 var routes = require('./routes/index');
 var feed = require('./routes/feed');
 
 
-
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // *Make our db accessible to our router
-app.use(function(req, res, next) {
-	req.db = db;
-	next();
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
 });
+
+
+// Facebook Graph Calls to Periodically Collect Data
+setInterval(function () {
+    graph
+        .get("1553840998179517/posts", function (err, res) {
+            console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+        });
+}, 5000);
+
 
 app.use('/', routes);
 app.use('/feed', feed);
 
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -55,23 +72,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
+
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
